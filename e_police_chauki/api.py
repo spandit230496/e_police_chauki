1119c297bb451b45ef236b4767f07bb08c4a2f2d
sandip.pandit <sandip.pandit@redsoftware.in>
2026-02-08 08:14:42 +0000

    add:workspace and html

--- e_police_chauki/api.py
+++ e_police_chauki/api.py
@@ -1,12 +1,22 @@
 import frappe
 
+
 @frappe.whitelist(allow_guest=True)
 def verify_case(case):
 
+    if not frappe.db.exists("Case File", case):
+        frappe.throw("Invalid Case Number")
+
     doc = frappe.get_doc("Case File", case)
 
-    return {
-        "case_id": doc.name,
-        "status": doc.current_status,
-        "created_on": doc.creation
-    }
+    html = f"""
+    <h2>Case Verification</h2>
+    <p><b>Case ID:</b> {doc.name}</p>
+    <p><b>Status:</b> {doc.current_status}</p>
+    <p><b>Created On:</b> {doc.creation}</p>
+    """
+
+    return frappe.respond_as_web_page(
+        title="Case Verification",
+        html=html
+    )
